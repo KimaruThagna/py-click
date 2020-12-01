d24169125eb387edbe78fa4bf8379758164ea7bd
Kimaru thagana <macbuntu@192.168.1.7>
2020-12-01 14:11:45 +0300

    add list datasets and export excel

--- click_cli.py
+++ click_cli.py
@@ -11,9 +11,21 @@ from utils import *
 @click.option('--list', help='Show available datasets', is_flag=True)
 def cli_app(id=None, path=None, excel=None, pdf=None,
             delete=None, describe=None, stats=None, list=None):
+    
     if list:
         list_datasets()
         for id, file in enumerate(list_datasets()):
             click.echo(f'id \t{id} \t file name\t{file} \n')
     if excel and id:
-        print(export_to_excel(int(id)))
+        click.echo(export_to_excel(int(id)))
+    if pdf and id:
+        click.echo(export_to_pdf(int(id)))
+    if describe and id:
+        click.echo(describe_dataset(int(id)))
+    if stats and id:
+        click.echo(single_dataset(int(id)))
+    if delete and id:
+        click.echo(delete_dataset(int(id)))
+    if path:
+        click.echo(upload_dataset(path))
+        
